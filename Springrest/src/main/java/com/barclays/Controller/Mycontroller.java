94ec6f3637b285263417da2eabceb2adb6b2894f
manis <manis@DESKTOP-T9BFMHJ>
2018-10-06 17:05:07 +0530

    m2 and m3

--- Springrest/src/main/java/com/barclays/Controller/Mycontroller.java
+++ Springrest/src/main/java/com/barclays/Controller/Mycontroller.java
@@ -23,4 +23,14 @@ public class Mycontroller {
 	{
 		System.out.println("method m1");
 	}
+	
+	public void m2()
+	{
+		System.out.println("method m2");
+	}
+	
+	public void m3()
+	{
+		System.out.println("method m3");
+	}
 }
